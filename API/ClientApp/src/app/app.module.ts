543d9e8dde8215657551c80c3f9f2b98ca763122
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-10-19 16:11:40 +0300

    Initial commit

---
+++ API/ClientApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HomeComponent} from "./home/home.component";
+import {NavMenuComponent} from "./nav-menu/nav-menu.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
