dbdd1159dcc99ad1cd208d74bf93f776cc0fc4ba
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-11-01 15:46:29 +0300

    add server filtering

--- API/Controllers/CitiesController.cs
+++ API/Controllers/CitiesController.cs
@@ -23,9 +23,12 @@ public class CitiesController : ControllerBase
         int pageIndex = 0,
         int pageSize = 10,
         string sortColumn = null,
-        string sortOrder = null)
+        string sortOrder = null,
+        string filterColumn = null,
+        string filterQuery = null)
     {
-        return await ApiResult<City>.CreateAsync(_context.Cities, pageIndex, pageSize, sortColumn, sortOrder);
+        return await ApiResult<City>.CreateAsync(_context.Cities,
+            pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
     }
 
     // GET: api/Cities/5
--- API/Data/ApiResult.cs
+++ API/Data/ApiResult.cs
@@ -15,7 +15,9 @@ public class ApiResult<T>
         int pageIndex,
         int pageSize,
         string sortColumn,
-        string sortOrder)
+        string sortOrder,
+        string filterColumn,
+        string filterQuery)
     {
         Data = data;
         PageIndex = pageIndex;
@@ -24,6 +26,8 @@ public class ApiResult<T>
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         SortColumn = sortColumn;
         SortOrder = sortOrder;
+        FilterColumn = filterColumn;
+        FilterQuery = filterQuery;
     }
     
     /// <summary>
@@ -71,6 +75,17 @@ public class ApiResult<T>
     /// Sorting Order ("ASC", "DESC" or null if none set)
     /// </summary>
     public string SortOrder { get; set; }
+    
+    /// <summary>
+    /// Filter Column name (or null if none set)
+    /// </summary>
+    public string FilterColumn { get; set; }
+    
+    /// <summary>
+    /// Filter Query string 
+    /// (to be used within the given FilterColumn)
+    /// </summary>
+    public string FilterQuery { get; set; }
 
     /// <summary>
     /// Pages a IQueryable source.
@@ -80,6 +95,8 @@ public class ApiResult<T>
     /// <param name="pageSize">The actual size of each page</param>
     /// <param name="sortColumn">The sorting column name</param>
     /// <param name="sortOrder">The sorting order ("ASC" or "DESC")</param>
+    /// <param name="filterColumn">The filtering column name</param>
+    /// <param name="filterQuery">The filtering query (value to lookup)</param>
     /// <returns>
     /// A object containing the paged result 
     /// and all the relevant paging navigation info.
@@ -90,8 +107,15 @@ public class ApiResult<T>
         int pageIndex,
         int pageSize,
         string sortColumn = null,
-        string sortOrder = null)
+        string sortOrder = null,
+        string filterColumn = null,
+        string filterQuery = null)
     {
+        if (!string.IsNullOrEmpty(filterColumn) && !string.IsNullOrEmpty(filterQuery) && IsValidProperty(filterColumn))
+        {
+            source = source.Where(string.Format("{0}.Contains(@0)", filterColumn), filterQuery);
+        }
+        
         var count = await source.CountAsync();
 
         if (!string.IsNullOrEmpty(sortColumn) && IsValidProperty(sortColumn))
@@ -108,14 +132,14 @@ public class ApiResult<T>
 
         var data = await source.ToListAsync();
 
-        return new ApiResult<T>(data, count, pageIndex, pageSize, sortColumn, sortOrder);
+        return new ApiResult<T>(data, count, pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
     }
 
     /// <summary>
     /// Checks if the given property name exists
     /// to protect against SQL injection attacks
     /// </summary>
-    public static bool IsValidProperty(string propertyName, bool throwExceptionIfNotFound = true)
+    private static bool IsValidProperty(string propertyName, bool throwExceptionIfNotFound = true)
     {
         var prop = typeof(T).GetProperty(
             propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
