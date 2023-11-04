9b0cb82d35a4b330e1373acfc272922391c5d9a8
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-11-04 21:10:38 +0300

    add base service

---
+++ API/ClientApp/src/app/base/base.service.ts
@@ -0,0 +1,27 @@
+﻿import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiResult } from '../models/api-result.interface';
+
+export abstract class BaseService {
+  protected constructor(
+    protected http: HttpClient,
+    protected baseUrl: string
+  ) {
+  }
+
+  abstract getData<T>(
+    pageIndex: number,
+    pageSize: number,
+    sortColumn: string,
+    sortOrder: string,
+    filterColumn: string,
+    filterQuery: string): Observable<ApiResult<T>>;
+
+  abstract get<T>(id: number): Observable<T>;
+
+  abstract put<T>(item: T): Observable<T>;
+
+  abstract post<T>(item: T): Observable<T>;
+
+
+}
