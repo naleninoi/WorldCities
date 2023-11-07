e0825d01b873653dc9f6a4afe2e70fab812a0691
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2023-11-07 16:31:57 +0300

    add client auth module

--- API/ClientApp/src/app/app-routing.module.ts
+++ API/ClientApp/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
 import { CityEditComponent } from './city-edit/city-edit.component';
 import { CountryEditComponent } from './country-edit/country-edit.component';
+import { AuthorizeGuard } from '../api-authorization/authorize.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
-  { path: 'city/:id', component: CityEditComponent },
+  { path: 'city/:id', component: CityEditComponent, canActivate: [AuthorizeGuard] },
   { path: 'city', component: CityEditComponent },
   { path: 'countries', component: CountriesComponent },
-  { path: 'country/:id', component: CountryEditComponent },
+  { path: 'country/:id', component: CountryEditComponent, canActivate: [AuthorizeGuard] },
   { path: 'country', component: CountryEditComponent },
 ];
 
--- API/ClientApp/src/app/app.module.ts
+++ API/ClientApp/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component';
 import {NavMenuComponent} from './nav-menu/nav-menu.component';
 import { CitiesComponent } from './cities/cities.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BASE_URL } from './_config/app.config';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -14,6 +14,8 @@ import { AngularMaterialModule } from './angular-material.module';
 import { CountriesComponent } from './countries/countries.component';
 import { CityEditComponent } from './city-edit/city-edit.component';
 import { CountryEditComponent } from './country-edit/country-edit.component';
+import { ApiAuthorizationModule } from '../api-authorization/api-authorization.module';
+import { AuthorizeInterceptor } from '../api-authorization/authorize.interceptor';
 
 @NgModule({
   declarations: [
@@ -32,10 +34,12 @@ import { CountryEditComponent } from './country-edit/country-edit.component';
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ApiAuthorizationModule
   ],
   providers: [
-    { provide: BASE_URL, useValue: 'https://localhost:7105/api/', deps: [] }
+    { provide: BASE_URL, useValue: 'https://localhost:7105/api/', deps: [] },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
